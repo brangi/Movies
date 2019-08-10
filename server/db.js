4442c658a6fa190501ceee6acef7e9f43e30f99d
Gibran <brangi000@gmail.com>
2019-08-10 11:55:14 -0400

    Create sql scripts for table and user

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const mysql      = require('mysql');
 const db = mysql.createConnection({
   host     : 'localhost',
-  user     : 'root',
-  password : 'password',
+  user     : 'movieuser',
+  password : 'testingpass',
   database : 'movies'
 });
 
